72964bcad3b62a27148fd771d1d7c2922ef7132e
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2020-01-06 16:26:30 -0400

    Initial Commit

---
+++ VOIPChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VOIPChallenge
+//
+//  Created by Jezreel de Oliveira Barbosa on 06/01/20.
+//  Copyright © 2020 Eldorado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
